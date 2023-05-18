4c4846d932c2da3061a93af3c9e58802f0655126
Amy Jin 金翠珠 <amy.jin@gaiaworks.cn>
2023-05-18 16:12:05 +0800

    4

--- pic/js/images.js
+++ pic/js/images.js
@@ -1,4 +1,4 @@
-var images = [
+var images1 = [
     {
         id: '陈菁华 王希明 刘可夫 黄健 章晓慧 盛志洪',
         // url: 'https://images.cnblogs.com/cnblogs_com/jcz1206/542960/o_230517125500_陈菁华 王希明 刘可夫 黄健 章晓慧 盛志洪.jpg'
@@ -126,3 +126,130 @@ var images = [
 //     n.url = encodeURI(n.url);
 // });
 // console.log('=====', images);
+var images = [
+    {
+        id: '陈菁华 王希明 刘可夫 黄健 章晓慧 盛志洪',
+        url: './images/陈菁华 王希明 刘可夫 黄健 章晓慧 盛志洪.jpg'
+    },
+    {
+        id: '陈菁华 王希明 刘可夫 黄健',
+        url: './images/陈菁华 王希明 刘可夫 黄健.jpg'
+    },
+    {
+        id: '程长利',
+        url: './images/程长利.jpg'
+    },
+    {
+        id: '段旭，魏秀娥，刘海艳',
+        url: './images/段旭，魏秀娥，刘海艳.jpg'
+    },
+    {
+        id: '顾昕雅，曹翔，徐运',
+        url: './images/顾昕雅，曹翔，徐运.jpg'
+    },
+    {
+        id: '顾玉华，环林林，王乐红，杨群，赵晨',
+        url: './images/顾玉华，环林林，王乐红，杨群，赵晨.jpg'
+    },
+    {
+        id: '李成刚 徐运',
+        url: './images/李成刚 徐运.jpg'
+    },
+    {
+        id: '李会琴，曹翔，徐运',
+        url: './images/李会琴，曹翔，徐运.jpg'
+    },
+    {
+        id: '李梦璐 刘畅',
+        url: './images/李梦璐 刘畅.jpg'
+    },
+    {
+        id: '马莉',
+        url: './images/马莉.jpg'
+    },
+    {
+        id: '倪恒，贾振宇，卢光东，刘兴龙，王斌，施海彬，刘圣，赵林波',
+        url: './images/倪恒，贾振宇，卢光东，刘兴龙，王斌，施海彬，刘圣，赵林波.jpg'
+    },
+    {
+        id: '钱青  陆敏智',
+        url: './images/钱青  陆敏智.jpg'
+    },
+    {
+        id: '汤瑶 朱奕 王彤',
+        url: './images/汤瑶 朱奕 王彤.jpg'
+    },
+    {
+        id: '陶媛媛 刘传道 陈庆梅 韩立影 钮晟佳 顾秦菲 蔡萍 王海波',
+        url: './images/陶媛媛 刘传道 陈庆梅 韩立影 钮晟佳 顾秦菲 蔡萍 王海波.jpg'
+    },
+    {
+        id: '田雪勤 赵格格 蒋婧 王嘉浩 王英歌',
+        url: './images/田雪勤 赵格格 蒋婧 王嘉浩 王英歌.jpg'
+    },
+    {
+        id: '王嘉浩 蒋婧 赵格格 田雪勒 王英歌',
+        url: './images/王嘉浩 蒋婧 赵格格 田雪勒 王英歌.jpg'
+    },
+    {
+        id: '王星智',
+        url: './images/王星智.jpg'
+    },
+    {
+        id: '王星智',
+        url: './images/王星智1.jpg'
+    },
+    {
+        id: '谢怡 黄震乾',
+        url: './images/谢怡 黄震乾.jpg'
+    },
+    {
+        id: '许雅颖 瞿小锋 高阳',
+        url: './images/许雅颖 瞿小锋 高阳.jpg'
+    },
+    {
+        id: '许雅颖 瞿小锋 高阳',
+        url: './images/许雅颖 瞿小锋 高阳1.jpg'
+    },
+    {
+        id: '阎雨婷 赵义 瞿航 周秋麟 王苇',
+        url: './images/阎雨婷 赵义 瞿航 周秋麟 王苇.jpg'
+    },
+    {
+        id: '于丽莎',
+        url: './images/于丽莎.jpg'
+    },
+    {
+        id: '袁晓芳，张一',
+        url: './images/袁晓芳，张一.jpg'
+    },
+    {
+        id: '张敏',
+        url: './images/张敏.jpg'
+    },
+    {
+        id: '赵超 陈国芳 刘薇薇 徐辉 王琛',
+        url: './images/赵超 陈国芳 刘薇薇 徐辉 王琛.jpg'
+    },
+    {
+        id: '赵超 陈国芳 徐辉 刘薇薇 王琛',
+        url: './images/赵超 陈国芳 徐辉 刘薇薇 王琛.jpg'
+    },
+    {
+        id: '郑慧芬 田琦 黄丹青',
+        url: './images/郑慧芬 田琦 黄丹青.jpg'
+    },
+    {
+        id: '程长利',
+        url: './images/程长利.jpg'
+    },
+    {
+        id: '周龙江 张鑫',
+        url: './images/周龙江 张鑫.jpg'
+    },
+    {
+        id: '周美丽',
+        url: './images/周美丽.jpg'
+    },
+    
+]
